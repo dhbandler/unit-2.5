39ea5d31fa12125b862e3e584cdd735dad6588dd
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-07 08:24:28 -0500

    Updated from Brython Server: 2/7/2018 8:24:27 AM

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -4,14 +4,23 @@
 
 from ggame import *
 
-red = Color(0xFF0000,1) #this is color red
-black = Color(0x000000,1) #this is the color black
+red = Color(0xFF0000,1) #this is color red, 1 is opacity
+black = Color(0x000000,1) #color black
+green = Color(0x00FF00,1) #color green
+blue = Color(0x0000FF,1) #color blue
 
 blackOutline = LineStyle(1,black)
 
 redRectangle = RectangleAsset(200, 100, blackOutline, red) #(width, height, outline type, fill)
+blueCircle = CircleAsset(100, blackOutline, blue) #radius, outline, fill
+greenEllipse = EllipseAsset(100,50,blackOutline, green) #width, height, outline, fill
+blackLine = LineAsset(50,160, blackOutline) #x endpoint (for ending), y endpoint (for ending starting from top left), linestyle
+redTriangle = PolygonAsset([(0,150), (120,230), (60,450)], blackOutline, red) #endpoints (list), outline, fill
 
 Sprite(redRectangle)
-
+Sprite(blueCircle,(300,200)) #sprite and coordinates
+Sprite(greenEllipse,(150,100))
+Sprite(blackLine)
+Sprite(redTriangle)
 
 App().run()
