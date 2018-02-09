e4e1b17b6e7854a2d58d2050883fe8952dfbcfca
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-09 08:31:50 -0500

    Update name.py

--- name.py
+++ name.py
@@ -4,13 +4,18 @@
 
 from ggame import *
 
-name = input("input your name here")
-code = input("input your Hexedecimal color code here")
+name = input("input your name here ")
+code = input("input your Hexedecimal color code here ")
+black = Color(0x000000, 1)
 
-selectedColor = Color(code, 1)
+selectedColor = Color("0x"+code, .5)
 
-text = TextAsset(name, fill = selectedColor, style = "bold 40pt Georgia")
+selectedOutline = LineStyle(1,selectedColor)
+
+text = TextAsset(name, fill = black, style = "bold 40pt Georgia")
+background = RectangleAsset(1000, 1000, selectedOutline, selectedColor)
 
 Sprite(text)
+Sprite(background)
 
 App().run()
