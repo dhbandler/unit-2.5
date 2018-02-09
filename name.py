aac72b42218cfb90d7a5633bb2f23d039d0b61fb
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-09 08:22:43 -0500

    Update name.py

--- name.py
+++ name.py
@@ -5,4 +5,12 @@
 from ggame import *
 
 name = input("input your name here")
-code = input("
+code = input("input your Hexedecimal color code here")
+
+selectedColor = Color(code, 1)
+
+text = TextAsset(name, fill = selectedColor, style = "bold 40pt Georgia")
+
+Sprite(text)
+
+App().run()
