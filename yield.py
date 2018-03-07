97e8c968469bcbccd2d6cf1b10252be9c3e5436d
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-07 08:22:24 -0500

    Updated from Brython Server: 3/7/2018 8:22:23 AM

--- yield.py
+++ yield.py
@@ -12,13 +12,13 @@ blackOutline = LineStyle(1, black)
 redOutline = LineStyle(1, red)
 whiteOutline = LineStyle(1, white)
 
-triangle1 = PolygonAsset([(0,1), (200,1), (100,173.205)], blackOutline, white)
-triangle2 = PolygonAsset([(5,1), (195,1), (100, 164.205)], redOutline, red)
+triangle1 = PolygonAsset([(0,0), (200,0), (100,173.205)], blackOutline, white)
+triangle2 = PolygonAsset([(5,0), (195,0), (100, 164.205)], redOutline, red)
 triangle3 = PolygonAsset([(50,50), (150,50), (100,129.205)], whiteOutline, white)
 text = TextAsset("YIELD", fill = red, style = "bold 10pt Arial")
 
-Sprite(triangle1)
-Sprite(triangle2, (5,5))
+Sprite(triangle1, (0,1))
+Sprite(triangle2, (5,4))
 Sprite(triangle3, (50,50))
 Sprite(text, (80,75))
 
